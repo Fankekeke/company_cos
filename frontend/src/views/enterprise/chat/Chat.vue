6e3d7e3ee2749c96d1fcf4ddfeec9c618c3fd598
Fankekeke <1258846421@qq.com>
2023-02-21 23:37:58 +0800

    fix -添加聊天组件

---
+++ frontend/src/views/enterprise/chat/Chat.vue
@@ -0,0 +1,39 @@
+<template>
+  <a-row style="width: 100%;">
+    <a-col :span="4">
+      <a-menu mode="inline" style="width: 100%;margin-top: 30px">
+        <a-menu-item key="mail" style="height: 80px;margin: 0 auto">
+          <a-avatar
+            :size="46"
+            icon="user"
+            :src="'http://127.0.0.1:9527/imagesWeb/SA1676716298671.jpg'"
+          />
+          <span style="margin-left: 15px">孙笑川</span>
+        </a-menu-item>
+        <a-menu-item key="mail1" style="height: 80px">
+          <a-avatar
+            :size="46"
+            icon="user"
+            :src="'http://127.0.0.1:9527/imagesWeb/SA1676716298671.jpg'"
+          />
+          <span style="margin-left: 15px">孙笑川</span>
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- frontend/src/views/expert/Expert.vue
+++ frontend/src/views/expert/Expert.vue
@@ -2,36 +2,82 @@
   <a-card style="width: 63%;margin: 0 auto;padding-top: 30px">
     <i style="font-size: 23px;margin: 30px;">专家列表</i>
     <div style="margin: 30px">
+      <a-input-search placeholder="input search text" style="width: 300px;float: right" @search="onSearch"/>
       <a-list
+        style="margin-top: 15px;clear: both"
         class="demo-loadmore-list"
         :loading="loading"
         item-layout="horizontal"
         :data-source="expertList"
       >
         <a-list-item slot="renderItem" slot-scope="item, index">
-          <a slot="actions">edit</a>
-          <a slot="actions">more</a>
-          <a-list-item-meta
-            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-          >
-            <a slot="title" href="https://www.antdv.com/">{{ item.name }}</a>
+          <a slot="actions" @click="chat(item)">沟通</a>
+          <a slot="actions" @click="detailView(item)">详情</a>
+          <a-list-item-meta>
+            <div slot="description" style="font-family: SimHei;font-size: 14px;margin-top: 20px">
+              <a-row :gutter="20">
+                <a-col :span="6"><span>民族：</span>
+                  {{ item.nationality }}
+                </a-col>
+                <a-col :span="6"><span>性别：</span>
+                  {{ item.sex }}
+                </a-col>
+                <a-col :span="6"><span>政治面貌：</span>
+                  {{ item.politicalStatus }}
+                </a-col>
+                <a-col :span="6"><span>专业方向一级：</span>
+                  {{ item.levelOne }}
+                </a-col>
+                <br/>
+                <br/>
+                <a-col :span="6"><span>籍贯：</span>
+                  {{ item.nativePlace }}
+                </a-col>
+                <a-col :span="6"><span>工作单位：</span>
+                  {{ item.employer }}
+                </a-col>
+                <a-col :span="6"><span>职务：</span>
+                  {{ item.position }}
+                </a-col>
+                <a-col :span="6"><span>专业方向二级：</span>
+                  {{ item.levelTwo }}
+                </a-col>
+                <br/>
+                <br/>
+              </a-row>
+            </div>
+            <a slot="title" style="font-size: 20px;font-family: SimHei">{{ item.name }}</a>
             <a-avatar
+              shape="square"
+              :size="126"
+              icon="user"
               slot="avatar"
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+              :src="'http://127.0.0.1:9527/imagesWeb/'+item.images"
             />
           </a-list-item-meta>
-          <div>content</div>
         </a-list-item>
       </a-list>
     </div>
+    <expert-view
+      @close="handleExpertViewClose"
+      :expertShow="expertView.visiable"
+      :expertData="expertView.data">
+    </expert-view>
   </a-card>
 </template>
 
 <script>
+import ExpertView from '../admin/expert/ExpertProductView.vue'
+
 export default {
   name: 'Expert',
+  components: {ExpertView},
   data () {
     return {
+      expertView: {
+        visiable: false,
+        data: null
+      },
       loading: false,
       data: [],
       expertList: []
@@ -41,6 +87,16 @@ export default {
     this.getEnterpriseList()
   },
   methods: {
+    onSearch (value) {
+      this.getEnterpriseList(value)
+    },
+    detailView (value) {
+      this.expertView.data = value
+      this.expertView.visiable = true
+    },
+    handleExpertViewClose () {
+      this.expertView.visiable = false
+    },
     getEnterpriseList (key) {
       if (key === undefined || key === null) {
         key = ''
